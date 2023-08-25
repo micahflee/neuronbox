3c4ff513dd1dc3c9579797ff9491db7cb0cd3bca
Micah Lee <micah@micahflee.com>
2023-08-25 19:58:44 -0400

    Got a browse file dialog to pop up!

--- frontend/src/Pages/Transcribe.vue
+++ frontend/src/Pages/Transcribe.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
         <h1>Transcribe Audio</h1>
+        <button @click="openFileDialog">Browse File</button>
     </div>
 </template>
   
 <script setup>
+import { invoke } from '@tauri-apps/api/tauri'
+
+async function openFileDialog() {
+    invoke('select_file').then(response => {
+        if (response.file_path) {
+            console.log("File path selected:", response.file_path);
+        } else {
+            console.log("No file was selected.");
+        }
+    });
+}
 </script>
-  
+
 <style scoped></style>
   
--- native/src-tauri/src/main.rs
+++ native/src-tauri/src/main.rs
@@ -1,15 +1,42 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use std::sync::mpsc;
+use tauri::api::dialog::FileDialogBuilder;
+
+#[derive(serde::Deserialize)]
+struct Params;
+
+#[derive(serde::Serialize)]
+struct Response {
+    file_path: Option<String>,
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![select_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn select_file() -> String {
+    println!("select_file() called");
+    let (tx, rx) = mpsc::channel();
+
+    tokio::task::spawn_blocking(|| {
+        FileDialogBuilder::new().pick_file(move |file_path| {
+            println!("pick_file() called");
+            let path_str = file_path
+                .map(|p| p.to_str().unwrap().to_string())
+                .unwrap_or_else(|| "".to_string());
+            tx.send(path_str).unwrap();
+        });
+    }).await.unwrap();
+
+    let received_path = rx.recv().unwrap();
+    if received_path.is_empty() {
+        "".to_string()
+    } else {
+        received_path
+    }
 }
